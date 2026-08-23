a401fcc147a66ce06328e94763e991750823c0f7
Alex_Gitignore <sadistik871m@gmail.com>
2026-08-23 23:25:12 +0300

    feat: issue access and refresh tokens on registration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,21 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.getOrThrow('JWT_ACCESS_SECRET'),
+        signOptions: { expiresIn: config.getOrThrow('JWT_ACCESS_EXPIRES') },
+      }),
+    }),
+  ],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,10 +3,13 @@ import { CreateUserDto } from '../users/dto/create-user.dto';
 import { UsersService } from '../users/users.service';
 import { CreateUserData } from '../users/types/CreateUserData';
 import * as bcrypt from 'bcrypt';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '../users/entities/user.entity';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService, private readonly configService: ConfigService, private readonly jwtService: JwtService ) {}
   async register(user: CreateUserDto) {
     const userEmail = await this.usersService.findByEmail(user.email);
     const userLogin = await this.usersService.findByLogin(user.login);
@@ -23,6 +26,23 @@ export class AuthService {
       age: user.age,
       about: user.about,
     }
-    return await this.usersService.createUser(userToCreate);
+    const createdUser =  await this.usersService.createUser(userToCreate);
+
+    return this.generateTokens(createdUser);
+  }
+
+  async generateTokens(user: User) {
+    const payload = { sub: user.id, login: user.login };
+
+    const accessToken = await this.jwtService.signAsync(payload)
+    const refreshToken = await this.jwtService.signAsync(payload, {
+      secret: this.configService.getOrThrow('JWT_REFRESH_SECRET'),
+      expiresIn: this.configService.getOrThrow('JWT_REFRESH_EXPIRES'),
+    });
+
+    return {
+      access_token: accessToken,
+      refresh_token: refreshToken,
+    }
   }
 }
